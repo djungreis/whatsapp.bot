485c67c4a954eb6ee31e768d51a500dd074dda4d
djungreis <Rcsiny@gmail.com>
2026-01-04 01:58:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "carbot123";
 
 app.get("/", (req, res) => {
